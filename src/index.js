832fc9bdbe9222718e88b29cbbecae37c1ed96e3
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2019-10-11 15:12:36 -0500

    instalación de Redux(provider, agregar carpetas actions, reducers)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render(
+    //Redux necesita encapsular la app dentro de un provider 
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('app'));
