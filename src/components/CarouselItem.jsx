7065d3f56fd8123a9a67a5caf2234e1d96cf604d
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2019-10-12 01:00:47 -0500

    Validaciones para el botón de agregar y eliminar favoritos

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -8,7 +8,7 @@ import plusIcon from '../assets/static/plus-icon.png';
 import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = (props) => {
-  const { id, cover, title, year, contentRating, duration } = props;
+  const { id, cover, title, year, contentRating, duration, isList } = props;
   //Función que maneja el guardado hacia nuestros favoritos
   const handleSetFavorite = () => {
     props.setFavorite({
@@ -24,16 +24,20 @@ const CarouselItem = (props) => {
       <div className="carousel-item__details">
         <div>
           <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
-          <img className="carousel-item__details--img"
-            src={plusIcon}
-            alt="Plus Icon"
-            onClick={handleSetFavorite}
-          />
-          <img className="carousel-item__details--img"
-            src={removeIcon}
-            alt="Remove Icon"
-            onClick={() => handleDeleteFavorite(id)}
-          />
+
+          {
+            isList ?
+              <img className="carousel-item__details--img"
+                src={removeIcon}
+                alt="Remove Icon"
+                onClick={() => handleDeleteFavorite(id)}
+              /> :
+              <img className="carousel-item__details--img"
+                src={plusIcon}
+                alt="Plus Icon"
+                onClick={handleSetFavorite}
+              />
+          }
         </div>
         <p className="carousel-item__details--title">{title}</p>
         <p className="carousel-item__details--subtitle">
