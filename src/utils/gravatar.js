ff7b15abf373e72287fedc18f800470e9d9f1790
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2019-10-12 02:32:23 -0500

    Creando un servicio para Gravatar

---
+++ src/utils/gravatar.js
@@ -0,0 +1,8 @@
+import md5 from 'md5';
+
+const gravatar = (email) => {
+    const base = 'https//gravatar.com/avatar/';
+    //Formatear email, trim para eliminar espacios, toLowerCase para pasarlo todo a minusculas
+    const formatteEmail = (email).trim().toLowerCase();
+    const hash = md5(formatteEmail, { encoding: 'binary' }) //no podemos mandar @, tenemos que convertirla a un formato más binario 
+};
