7734826232ea7c07f0507811c93bdba2fc906e06
SilviaAlarcon <silvia.alarcon.or@gmail.com>
2019-10-12 03:05:34 -0500

    Uso de gravatar en el proyecto

--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -1,9 +1,11 @@
 import md5 from 'md5';
 
 const gravatar = (email) => {
-    const base = 'https//gravatar.com/avatar/';
+    const base = 'https://gravatar.com/avatar/';
     //Formatear email, trim para eliminar espacios, toLowerCase para pasarlo todo a minusculas
-    const formatteEmail = (email).trim().toLowerCase();
-    const hash = md5(formatteEmail, { encoding: 'binary' }) //no podemos mandar @, tenemos que convertirla a un formato más binario 
-    return `${base} ${hash}`
+    const formattedEmail = (email).trim().toLowerCase();
+    const hash = md5(formattedEmail, { encoding: 'binary' }) //no podemos mandar @, tenemos que convertirla a un formato más binario 
+    return `${base}${hash}`
 };
+
+export default gravatar; 
